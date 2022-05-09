2de3ec1a17a0d9f31a5e126c5dc1d77314ac2771
Alberto Del Angel <rockstar4400@gmail.com>
2022-05-08 23:51:19 -0500

    Introduction ReadWrite

---
+++ 1.INTRODUCTION/ReadWrite/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const http = require('http');
+const url = require('url');
+
+//////////////////// ReadWrite ////////////////////////////////
+// Bloking, synchronous way
+// const textIn = fs.readFileSync('./input.txt', 'utf-8');
+// //console.log(textIn);
+// const textOut = `This is what we know: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./output.txt', textOut);
+// console.log('File written');
+
+// No-blocking, asynchrous way
+// fs.readFile('./start.txt', 'utf-8', (err, data1)=> {
+    
+//     if(err) return console.log('ERROR!')
+
+//     fs.readFile(`./${data1}.txt`, 'utf-8', (err, data2)=> {
+//         console.log(data2);
+//         fs.readFile(`./append.txt`, 'utf-8', (err, data3)=> {
+//             console.log(data3);
+
+//             fs.writeFile('./final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('Written')
+//             })
+//         });
+//     });
+// });
+
+// console.log('Will read file!');
+
+//////////////////// Server ////////////////////////////////
+const server = http.createServer((req, resp) => {
+    console.log(req.url);
+    resp.end('Hello from the server!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listen')
+});
