96e2d0acc8b08dfeb1c395b2dd073b6cd61a8b5e
Alberto Del Angel <rockstar4400@gmail.com>
2022-05-17 21:33:38 -0500

    Routes

--- 4.EXPRESS/app.js
+++ 4.EXPRESS/app.js
@@ -1,49 +1,27 @@
-const fs = require('fs');
+
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// 1) Middlewares
+
+app.use(morgan('dev'));
 app.use(express.json());
-
-// app.get('/', (req, res) => {
-//     res.status(200).json({message: 'Hello from the server!', app: 'Server'});
-// });
-
-// app.post('/', (req, res) => {
-//     res.send('Post endpoint');
-// });
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-
-app.get('/api/v1/tours',(req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours
-        }
-    })
+app.use((req, res, next) => {
+    console.log('Hello middleware');
+    next();
 });
+app.use((req, res, next) =>{
+    req.requestTime = new Date().toISOString();
+    next();
+})
 
-app.post('/api/v1/tours',(req,res) => {
-    //console.log(req.body);
-    const newId = tours[tours.length-1].id+1;
-    const newTour = Object.assign({id:newId}, req.body);
+// Routes
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err =>{
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        })
-    });
-//asd
-    
-});
-
-const port = 3000;
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`)
-});
+module.exports = app;
