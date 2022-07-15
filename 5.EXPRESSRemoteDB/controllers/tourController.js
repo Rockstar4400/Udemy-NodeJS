e1842d2356e349330d934591c268f5b163248d90
Alberto Del Angel <rockstar4400@gmail.com>
2022-07-15 13:38:59 -0500

    Import Delete data DB Remote

--- 5.EXPRESSRemoteDB/controllers/tourController.js
+++ 5.EXPRESSRemoteDB/controllers/tourController.js
@@ -2,12 +2,44 @@ const fs = require('fs');
 const Tour = require('./../models/tourModel');
 
 
-exports.getAllTours = (req, res) => {
+exports.getAllTours = async (req, res) => {
+
+    try{
+        const tours = await Tour.find();
     
+        res.status(200).json({
+            status: 'success',
+            result: tours.length,
+            data: {
+                tours
+            }
+        })
+    }
+    catch(err){
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
 };
 
-exports.getTour = (req, res) => {
-
+exports.getTour = async (req, res) => {
+    try{
+        const tour = await Tour.findById(req.params.id);
+    
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        })
+    }
+    catch(err){
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
 };
 
 exports.createTour = async (req,res) => {
@@ -28,21 +60,46 @@ exports.createTour = async (req,res) => {
     }
 };
 
-exports.updateTour = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated>'
-        }
-    })
+exports.updateTour = async (req, res) => {
+
+    try{
+        const tour = 
+        await Tour.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true,
+            runValidators: true}
+        );
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        })
+    }
+    catch(err){
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
 };
 
-exports.deleteTour = (req, res) => {
+exports.deleteTour = async (req, res) => {
     
-    
-
-    res.status(204).json({
-        status: 'success',
-        data: null
-    })
+    try{
+        await Tour.findByIdAndDelete(req.params.id);
+        
+        res.status(204).json({
+            status: 'success',
+            data: null
+        })
+    }
+    catch(err){
+        res.status(404).json({
+            status: 'faul',
+            message: err
+        })
+    }
 };
